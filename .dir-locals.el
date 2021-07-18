83c8c98979c11f3f97aa7cf362dcbb248dd086ef
Pascal J. Bourguignon <pjb@informatimago.com>
2021-07-18 11:05:46 +0200

    Added .editorconfig for WorkingCopy and updated .dir-local.el for Emacs.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,9 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((lisp-mode . ((tab-width . 8)))
- (c-mode . ((tab-width . 8)))
- (t . ((tab-width . 10))))
+((lisp-mode       . ((tab-width . 8) (indent-tabs-mode . t) (show-trailing-whitespace . t)))
+ (emacs-lisp-mode . ((tab-width . 8) (indent-tabs-mode . t) (show-trailing-whitespace . t)))
+ (python-mode     . ((tab-width . 8) (indent-tabs-mode . t) (show-trailing-whitespace . t)))
+ (c-mode          . ((tab-width . 8) (indent-tabs-mode . t) (show-trailing-whitespace . t)))
+ (asm-mode        . ((tab-width . 8) (indent-tabs-mode . t) (show-trailing-whitespace . t)))
+ (t               . ((tab-width . 10))))
